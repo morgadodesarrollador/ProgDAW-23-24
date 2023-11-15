748bf8a5e409819d7c7fad1799b701135e26370c
morgadodesarrollador <morgadodesarrollador@gmail.com>
2023-11-15 14:17:08 +0100

    ApiJSON

---
+++ JAVA/Conceptos/ApiJson/src/Main.java
@@ -0,0 +1,8 @@
+import apiJson.Users;
+public class Main {
+    public static void main(String[] args) {
+
+        Users usuarios = new Users();
+        usuarios.metodo1();
+    }
+}
---
+++ JAVA/Conceptos/ApiJson/src/apiJson/Pokemon.java
@@ -0,0 +1,4 @@
+package apiJson;
+
+public class Pokemon {
+}

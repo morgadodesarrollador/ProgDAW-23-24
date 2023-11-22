4458854cf2a5c24f995201f3013e5bd852534a48
morgadodesarrollador <morgadodesarrollador@gmail.com>
2023-11-22 14:28:15 +0100

    plugin J2THML

--- JAVA/Conceptos/HTML/src/html/Pagina.java
+++ JAVA/Conceptos/HTML/src/html/Pagina.java
@@ -1,6 +1,7 @@
 package html;
 import j2html.tags.Tag;
 import j2html.tags.specialized.HeadTag;
+import j2html.tags.specialized.TableTag;
 import j2html.tags.specialized.UlTag;
 
 import j2html.tags.specialized.HtmlTag;
@@ -13,6 +14,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Pagina {
+
 
     public  void make1(){
         HtmlTag contenido =
@@ -42,19 +44,22 @@ public class Pagina {
                                 ),
                             section()
                                     .with(
-                                       this.crearUsuarios()))
-                                    );
+                                       this.crearUsuariosUL(),
+                                       this.crearUsuariosTBL()
+                                    )
+                      )
+             );
 
 
 
-         String rendered = "<!DOCTYPE html>\n" + content.render();
+         String rendered = "<!DOCTYPE html>" + content.render();
          //String rendered = document().render() + content.render();
          this.makeFileHtml(rendered);
 
     }
     private void  makeFileHtml(String rendered) {
         try {
-            File file = new File("index1.html");
+            File file = new File("index.html");
             FileWriter out = new FileWriter(file);
             out.write(rendered);
             out.close();
@@ -67,7 +72,6 @@ public class Pagina {
                 .with(
                     meta().attr("charset","utf-8"),
                     title("Mi primera pagina j2html en Java"),
-                    //link().withRel("stylesheet").withHref("estilo.css"),
                     link().attr("rel","stylesheet")
                           .attr("type", "text/css")
                           .attr("href", "./estilos.css")
@@ -79,19 +83,42 @@ public class Pagina {
                 link().withRel("stylesheet").withHref("estilo.css")
             );*/
     }
-    private  Tag<UlTag> crearUsuarios() {
+
+    private  Tag<TableTag> crearUsuariosTBL() {
         ArrayList<String> listaNombres = new ArrayList<String>();
         listaNombres.add("Jose");
         listaNombres.add("Antonio");
         listaNombres.add("Rocio");
         listaNombres.add("Marta");
+        listaNombres.add("Ana");
         return
-                ul(each(listaNombres, nombre
-                           -> li(nombre)
-                       )
-                   );
+           table(
+                   each(listaNombres, nombre
+                    -> tr().with(
+                            td(nombre),
+                            td(nombre),
+                            td(nombre)
+                           )
+                   )
+
+           ) ;
 
 
+    }
+    private  Tag<UlTag> crearUsuariosUL() {
+        ArrayList<String> listaNombres = new ArrayList<String>();
+        listaNombres.add("Jose");
+        listaNombres.add("Antonio");
+        listaNombres.add("Rocio");
+        listaNombres.add("Marta");
+        listaNombres.add("Ana");
+
+        return
+                ul(
+                    each(listaNombres, nombre
+                       -> li(nombre).attr("class", "opcion")
+                   )
+                ).attr("class", "area");
 
     }
 

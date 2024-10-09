59c3f155febe52d28c104466fc7150c447fd5d0d
morgadodesarrollador <morgadodesarrollador@gmail.com>
2024-10-09 09:26:05 +0200

    Métodos que devuelven un valor

--- JAVA/24-25/Iniciales/ejercicios/Empleado24.java
+++ JAVA/24-25/Iniciales/ejercicios/Empleado24.java
@@ -13,8 +13,8 @@ public class Empleado24 {
     public static double salarioNeto; //SNeto = SBr - SRetencion
     
     //pq se necesitan en el cálculo y en la visualización
-    public static int salarioBase;
-    public static double irpf; //0.25 o 0.20
+    // public static int salarioBase;
+    // public static double irpf; //0.25 o 0.20
     public static int salarioBruto; // SBr = SB + DDietas 
     public static int dietas; //SDietas --> ndias * 30€
     public static double retencion; //SRetencion = SBr * irpf
@@ -43,50 +43,60 @@ public class Empleado24 {
         Empleado24.estadoCivil = Empleado24.entrada.next().toUpperCase().charAt(0);
     }
 
-    public static void calcularSalario(){
-        
-        // int salarioBase;
-        // int salarioBruto;
-        // double irpf;
-        // double retencion;
-
-        int dietas;
-        //fijar salario base
+    public static int calcularSBase(){
+        int sbase = 0;
         switch (Empleado24.cargo){
-            case 1: 
-                Empleado24.salarioBase = 950;
+            case 1:
+                sbase = 950;
+                // Empleado24.salarioBase = 950;
                 break;
             case 2: 
-                Empleado24.salarioBase = 1200;
+                // Empleado24.salarioBase = 1200;
+                sbase = 1200;
                 break;
             case 3: 
-                Empleado24.salarioBase = 1600;
+                sbase = 1600;
+                // Empleado24.salarioBase = 1600;
                 break;
         }
+        return sbase;
+        
+    }
 
+    public static double calcularIRPF(){
         //fijar irpf
+        double irpf = 0;
         switch (Empleado24.estadoCivil){
             case 'S':
-                Empleado24.irpf = 0.25;
+                irpf = 0.25;
+                // Empleado24.irpf = 0.25;
                 break;
             case 'C':
-                Empleado24.irpf = 0.2;
+                irpf = 0.2;
+                // Empleado24.irpf = 0.2;
                 break;
         }
+        return irpf;
+    }
 
+    public static void calcularSalario(){
+    
+        // int sbase = Empleado24.calcularSBase();
+        // Empleado24.calcularIRPF();
+        
         Empleado24.dietas = Empleado24.diasVisita * Empleado24.horaExtra;
-        Empleado24.salarioBruto = Empleado24.salarioBase + Empleado24.dietas;
-        Empleado24.retencion = Empleado24.salarioBruto * Empleado24.irpf;
+        Empleado24.salarioBruto = Empleado24.calcularSBase() + Empleado24.dietas;
+        Empleado24.retencion = Empleado24.salarioBruto * Empleado24.calcularIRPF();
         Empleado24.salarioNeto = Empleado24.salarioBruto - Empleado24.retencion;
     }
 
     public static void MostrarDatos(){
         System.out.println("------------------------");
-        System.out.printf("Salario Base %12d€\n", Empleado24.salarioBase);
+        System.out.printf("Salario Base %12d€\n", Empleado24.calcularSBase());
         System.out.printf("Dietas (%d viajes ) %2d€\n", Empleado24.diasVisita, Empleado24.dietas);
         System.out.println("------------------------");
         System.out.printf("Salario Bruto %12d€\n", Empleado24.salarioBruto);
-        System.out.printf("Retencion IRPF (%f ) %2f€\n", Empleado24.irpf*100, Empleado24.retencion);
+        System.out.printf("Retencion IRPF (%f ) %2f€\n", Empleado24.calcularIRPF()*100, Empleado24.retencion);
     }
 
 

540a47e23c9081bbde1dc099ca8286a003718b8a
morgadodesarrollador <morgadodesarrollador@gmail.com>
2024-10-31 00:15:41 +0100

    30/oct/24 - Arrays

---
+++ JAVA/24-25/Iniciales/conceptos/src/arrays/unaDim.java
@@ -0,0 +1,105 @@
+package arrays;
+import java.util.Scanner;
+//NO SE IMPORTA LA CLASE Math
+
+public class unaDim {
+    public static Scanner entrada = new Scanner(System.in); 
+    static int ALUMNOS = 5;
+    static int ASIGNATURAS = 4;
+    static int TRIMESTRES = 2;
+    String[] alumnos = new String[ALUMNOS];
+    
+    public static void main(String[] args){
+
+        //generar 10 notas aleatorias con dos decimales
+        unaDim.generarNotas();
+        //leer ALUMNOS notas por teclado
+        //unaDim.notasTeclado();
+    }
+
+    public static void generarNotas(){
+        double notas[] = new double[ALUMNOS];
+        double temp = 0.0;
+        double max = 0;
+        double min = 0;
+        double media = 0;
+        System.out.println("----- Generación de Notas ------");
+        for (int i=0; i<ALUMNOS; i++){
+            temp = Math.random()*10;
+            temp = Math.round(temp*100.00)/100.00;
+            notas[i] = temp;
+        }
+        System.out.println("----- Impresion de Notas ------");
+        for (int i = 0; i<ALUMNOS; i++){
+            System.out.println("nota " + i + " : " + notas[i]);
+        }
+        System.out.println("----- Informes de Resultados --------");
+        //suposicion
+        max = notas[0];
+        min = notas[0];
+        for (int i=0; i<ALUMNOS; i++){
+            media = media + notas[i];
+            if (notas[i]>max){
+                max = notas[i];
+            }
+            if (notas[i]<min){
+                min = notas[i];
+            }
+        }
+        System.out.println("Max :  " + max);
+        System.out.println("Min :  " + min);
+        media = media / ALUMNOS;
+        System.out.println("Media : " + media);
+        
+    }
+
+    public static void notasTeclado(){
+        
+        int[] notas = new int[ALUMNOS]; //0..4 indice's del array
+        System.out.println("----- Lectura de Notas ------");
+        for (int i = 0; i<ALUMNOS; i++){ //0..5 indice del for
+            System.out.print("Introduce nota " + i + " : ");
+            notas[i] = unaDim.entrada.nextInt();
+        }
+        System.out.println("----- Impresion de Notas ------");
+        for (int i = 0; i<ALUMNOS; i++){
+            System.out.print("nota " + i + " : " + notas[i]);
+        }
+
+    }
+
+    public static void explicacion(){
+        //almacenar array de notas (enteros) de 10 alumnos
+        int nota = 9; //v. monovaluada (2bytes)
+        // notas --> (array [] --> v.multivaluada)
+        //se usa el operador new() --> RESERVA EN T.COMPILACION(ESTATICA) 10 posiciones
+        //ENTERAS de memoria RAM int-->2 bytes --> notas ocupa 20 bytes
+        //----- DECLARACION DEL ARRAY (NO INICIALIZO)
+        int[] notas = new int[10]; //notas[0] ... notas[9]{0, ...., 0}
+        // int[] notas1 = { 2,5,7,1,6,8,9,10,0,4 };
+
+        //array 2D Bidimensional (tabla) (alumnos/asignaturas)
+        int[][] notas2 = new int[4][5]; //5f y 6c
+        notas2[0][0] = 3;
+        //...
+        notas2[0][4] = 7;
+        notas2[1][0] = 6;
+        //...
+        notas2[1][4] = 7;
+        //...
+        notas2[3][0] = 3;
+        notas2[3][4] = 7;
+        
+        //array 3D Tridimensional (cubo) (alumnos/asignaturas/trimestres)
+        int[][][] notas3 = new int[ALUMNOS][ASIGNATURAS][TRIMESTRES]; //5f, 6c, 3prof
+        notas3[2][1][2] = 5;
+    }
+   
+
+    
+
+    
+
+    
+} //cierra la clase
+
---
+++ JAVA/24-25/Iniciales/ejercicios/Aleatorios.java
@@ -0,0 +1,49 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Aleatorios {
+    public static Scanner entrada = new Scanner(System.in); 
+
+    public static void main(String[] args) throws Exception {
+
+        Aleatorios.ejercicio11();
+      
+  }
+
+  public static void ejercicio11(){
+    int suspensos = 0;
+    int aprobados = 0;
+    int bien = 0;
+    int notables = 0;
+    int sobresalientes = 0;
+    int mhonor = 0;
+    double nota = 0;
+    for (int i=0; i<=20; i++){ 
+        nota = Math.random()*10;
+        nota = Math.round(nota*100.00)/100.00;
+        System.out.print(nota + " ");
+        if (nota < 5){
+          suspensos++;
+        }else if ( (nota >= 5) && (nota < 6)){
+          aprobados++;
+        }else if ((nota >= 6) && (nota < 7)){
+          bien++;
+        }else if ((nota >= 7) && (nota < 9)){
+          notables++;
+        }else if ((nota >= 9) && (nota < 10)){
+          sobresalientes++;
+        }else{
+          mhonor++;
+        }
+    }
+    System.out.println("");
+    System.out.println("Suspensos: " + suspensos);
+    System.out.println("Aprobados: " + aprobados);
+    System.out.println("Bien: " + bien);
+    System.out.println("Notables: " + notables);
+    System.out.println("Sobresalientes: " + sobresalientes);
+    System.out.println("M.Honor: " + mhonor);
+
+  }
+}

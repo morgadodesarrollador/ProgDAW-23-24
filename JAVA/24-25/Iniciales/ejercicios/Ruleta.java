e75874e2bc15f0f9f017731eb2f00b75f3fd54b2
morgadodesarrollador <morgadodesarrollador@gmail.com>
2024-11-11 09:54:44 +0100

    Ordenacion de un vector de notas

---
+++ JAVA/24-25/Iniciales/ejercicios/Ruleta.java
@@ -0,0 +1,56 @@
+
+import java.util.Scanner;
+
+public class Ruleta {
+    public static Scanner entrada = new Scanner(System.in); 
+    static int TIRADAS = 10;
+    static int NPERSONAS = 5;
+    static String[] personas = { "Ana", "Juan", "Jose", "Maria", "Noelia" };
+    static double[] notas = new double[NPERSONAS];
+
+    public static void main(String[] args){
+        
+        generarVector();
+        imprimir();
+        System.out.println();
+        ordenar();
+       
+    }
+
+    static void generarVector(){
+        for (int i=0; i<NPERSONAS; i++){
+            double temp = Math.random()*10;
+            Ruleta.notas[i] = Math.round(temp*100.00)/100.00;
+        }
+    }
+    static int getMax(int pos){
+        double max = 0;
+        int pmax = 0;
+        for (int i=pos; i<NPERSONAS; i++){
+            if (Ruleta.notas[i]>max){
+                max = Ruleta.notas[i];
+                pmax=i;
+            }
+        }
+        return pmax;
+    }
+
+    static void ordenar(){
+        for (int i=0; i<NPERSONAS; i++){
+            int pmax = getMax(i);
+            double temp = Ruleta.notas[i];
+            Ruleta.notas[i] = Ruleta.notas[pmax];
+            Ruleta.notas[pmax] = temp;
+            imprimir();
+            System.out.print(" " + i + " <---> " + pmax  );
+            System.out.println();
+        }
+    }
+    static void imprimir(){
+        for (int i=0; i<NPERSONAS; i++){
+            System.out.print(Ruleta.notas[i] + " ");
+        }
+    }
+
+}
+

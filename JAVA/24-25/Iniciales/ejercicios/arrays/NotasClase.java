04c5a38b2ade233c1cc055a9a2f7762f043bb3cb
morgadodesarrollador <morgadodesarrollador@gmail.com>
2024-11-15 12:34:52 +0100

    Notas- 6,7,8

--- JAVA/24-25/Iniciales/ejercicios/arrays/NotasClase.java
+++ JAVA/24-25/Iniciales/ejercicios/arrays/NotasClase.java
@@ -17,15 +17,93 @@ public class NotasClase {
 
         NotasClase.GenerarNotas();
         NotasClase.ImprimirNotasTabla();
-        //1- Quien aprueba un modulo
+        //3- Quien aprueba un modulo
         // System.out.print("Modulo ? : ");
         // String modulo = NotasClase.entrada.nextLine(); 
         //imprimirDatos(quienAprueba("Redes"));
         
-        //2-. modulosAprueba(persona) ---> array Módulos que aprueban
-        imprimirDatos(modulosAprueba("Maria"));
+        //4-. modulosAprueba(persona) ---> array Módulos que aprueban
+        //imprimirDatos(modulosAprueba("Maria"));
         
-        
+        //5-. Promociona(persona) --> array Personas
+        // if (promociona("Maria")){
+        //     System.out.println("maria promociona");
+        // }else{
+        //     System.out.println("maria NO promociona");
+        // }
+        // // for (int j=0; j<ALUMNOS; j++){
+        // //     if (promociona(alumnos[j])){
+        // //         System.out.println(alumnos[j] + "promociona");
+        // //     }else{
+        // //         System.out.println(alumnos[j] + " NO promociona");
+        // //     }   
+        // // }
+        //6-. listadoModSuspensos()
+        // int[] nSuspensos = listarModSuspensos();
+        // for (int j=0; j<ALUMNOS; j++){
+        //     System.out.print(nSuspensos[j] + " ");
+        // }
+        //7,8,9 estadisitica(persona) --> [max, min, media]
+        estadisitica("Maria");
+
+    }
+    public static double[] estadisitica(String persona){
+        double[] datos = new double[3];
+        double max;
+        double min;
+        double media=0;
+        int JPer = getIndexPersona(persona);
+        max = NotasClase.notas[JPer][0];
+        min = NotasClase.notas[JPer][0];
+        for (int i=1; i<MODULOS; i++){
+            if (NotasClase.notas[JPer][i] > max){
+                max = NotasClase.notas[JPer][i];
+            }
+            if (NotasClase.notas[JPer][i] < min){
+                min = NotasClase.notas[JPer][i];
+            }
+            media= media + NotasClase.notas[JPer][i];
+        }   
+        // for (int j=0; j<ALUMNOS; j++){
+        //     max = NotasClase.notas[0][j];
+        //     min = NotasClase.notas[0][j];
+        //     for (int i=1; i<MODULOS; i++){
+        //         if (NotasClase.notas[i][j] > max){
+        //             max = NotasClase.notas[i][j];
+        //         }
+        //         if (NotasClase.notas[i][j] < min){
+        //             min = NotasClase.notas[i][j];
+        //         }
+        //     }   
+        datos[0] = max;
+        datos[1] = min;
+        datos[2] = media/MODULOS;
+        System.out.println(datos[0] + " " + datos[1] + " " + datos[2]);
+        return datos;
+    }
+    public static int[] listarModSuspensos(){
+        int[] numSupensos = new int[ALUMNOS];
+        for (int j=0; j<ALUMNOS; j++){
+            numSupensos[j] = 0; //contados susp del alumno a 0
+            for (int i=0; i<MODULOS; i++){
+                if (NotasClase.notas[j][i] < 5){
+                    numSupensos[j]++;
+                }
+            }
+        }
+        return numSupensos;
+    }
+    public static boolean promociona (String persona){
+        boolean aprueba = true; //sup que aprueba --> bandera
+        int Iper = getIndexPersona(persona);
+        if (Iper != -1){
+            for (int j=0; (j<MODULOS && aprueba ); j++){
+                if (NotasClase.notas[Iper][j] < 5){ 
+                    aprueba = false;
+                }
+            }
+        }
+        return aprueba;
     }
 
     public static String[] modulosAprueba(String persona){
@@ -33,7 +111,7 @@ public class NotasClase {
         String[] modulos = new String[MODULOS]; //respuesta
         if (Iper != -1){ //si esta la persona
             for (int i=0; i<MODULOS; i++){
-                if (NotasClase.notas[Iper][i] > 5){
+                if (NotasClase.notas[Iper][i] < 5){
                     modulos[i] = NotasClase.modulos[i]; 
                 }
             }

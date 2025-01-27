f30a640055da5fe6558222c91eae0882a535683b
morgadodesarrollador <morgadodesarrollador@gmail.com>
2025-01-27 10:25:50 +0100

    Descarga y guardar Posts

--- JAVA/24-25/Proyectos/gestpost/src/main/java/com/morgado/App.java
+++ JAVA/24-25/Proyectos/gestpost/src/main/java/com/morgado/App.java
@@ -1,6 +1,8 @@
 package com.morgado;
 //mias
 import com.morgado.Post;
+
+import java.io.FileWriter;
 //java
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,6 +12,7 @@ import java.lang.reflect.Type;
 
 //terceros
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
 //para hacer peticiones HTTP (get, post ...)  a una servidor web
@@ -22,21 +25,33 @@ import okhttp3.Response;
 public class App 
 {
     public static ArrayList<Post> LPost = new ArrayList<>();
+    public static Type postListType = new TypeToken<List<Post>>() {}.getType();
+
+    //prepara una instancia para realizar lecturas de consola
     public static  Scanner entrada = new Scanner(System.in); 
+    //prepara la una conexión inicial HTTP (a un servidor web Apache/nginx/Tomcat/iis/Express)
     public static OkHttpClient client = new OkHttpClient(); 
+    //url base de la API
     public static String urlRoot = "https://jsonplaceholder.typicode.com/"; 
+    //ruta del fichero pòst.json
+    public static String ruta = "/Users/morgado/Personal/Modulos/ProgDAW/JAVA/24-25/Proyectos/gestpost/public";
+
     public static void main( String[] args )
     {
         App.Menu();
     }
 
     public static void CargarPost(){
-        //Crea la REQUEST Http de tipo Get
+        //Crea la REQUEST Http de tipo Get para listar todos los Posts
         Request request = new Request.Builder()
                                 .url(App.urlRoot + "posts")
                                 .build();
         //Ejecutar la solicitud --> send
         System.out.println("Enviando ..."+ App.urlRoot + "posts");
+        //client.newCall(request).execute(): Lanza la peticion (request) hacia la api
+        //Api  :  (servidor web + sgbd) --> servidor fisico con su ip (dns)
+        //La Api recibe una REQUEST (petición) y devuelve una RESPONSE (respuesta JSON)
+        //me 
         try (Response response = client.newCall(request).execute() ) {
             if (!response.isSuccessful()){
                 System.out.println("Response Fallido");
@@ -45,14 +60,26 @@ public class App
             System.out.println("Conexión Establedida y Response ok");
             String responseBody = response.body().string();
             Gson gson = new Gson();
-            Type postListType = new TypeToken<List<Post>>() {}.getType();
-            List<Post> LPost = gson.fromJson(responseBody, postListType);
+            //genera la estructura del Post.json en base a la estructura de la clase Post
+            App.postListType = new TypeToken<List<Post>>() {}.getType();
+            //fromJson: transforma la respesta en formato json con su tipo (estructura)
+            //y genera un arraylist de java (lista de obejtos)
+            App.LPost = gson.fromJson(responseBody, postListType);
             for (Post post : LPost) {
-                System.out.println(post.getId());
-            }
-                        
-        }catch (IOException e){
-            e.printStackTrace();
+                System.out.println(post.getTitle());
+            }  
+        }catch (IOException e){ //IOException captura errores HTTP
+            e.printStackTrace(); //mensaje generico tipo "Ayuda SysAdmin"
+        }
+    }
+    public static void guardar(){
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try (FileWriter writer = new FileWriter(App.ruta+"/posts.json")) {
+            gson.toJson(App.LPost, writer);
+            System.out.println("Archivo posts.json creado correctamente.");
+        } catch (IOException e) {//IOException captura errores FILE
+            e.printStackTrace(); //mensaje generico tipo "Ayuda SysAdmin"
         }
     }
     public static void Menu(){
@@ -62,6 +89,7 @@ public class App
             System.out.println("\n--- Menú de Empleados ---");
             System.out.println("1. Cargar los Post");
             System.out.println("2. Cargar Usuarios");
+            System.out.println("6. Guardar Datos");
     
 
             System.out.println("0. Salir");
@@ -73,6 +101,10 @@ public class App
                 case 1:
                     App.CargarPost();
                     System.out.println("termino");
+                    break;
+                case 6:
+                    App.guardar();
+                    System.out.println("Guardando...");
                     break;
                 case 0:
                     System.out.println("Saliendo...");
